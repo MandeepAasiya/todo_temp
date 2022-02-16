2c4c4ea3cdc3f102bbf1a1095d6c05e3195d93dc
MandeepAasiya <70024836+MandeepAasiya@users.noreply.github.com>
2022-02-16 15:40:19 +0530

    Add files via upload

---
+++ init.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+#### List Maker ####
+#
+# Launch this Ruby file from the command line
+# to get started
+#
+
+#APP_ROOT = File.expand_path(File.dirname(__FILE__))
+
+#require_relative 'lib/controller'
+
+#controller = ListMaker::Controller.new
+#controller.launch!
+
+APP_ROOT = File.expand_path(File.dirname(__FILE__))
+
+require_relative 'lib/list_file'
+list = ListMaker::ListFile.new
+list.launch!
